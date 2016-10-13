d77fdd00086d699f747f64a54ee77cf23ea23ece
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-13 16:30:58 -0400

    I added a move hash, face card arrays, adjusted hand type method, added method to check if face card, added new game method, and finished the case statement.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -24,8 +24,8 @@ player_soft = {
   15 =>  ["H", "H", "DH", "DH", "DH", "H", "H", "H", "H", "H"],
   16 =>  ["H", "H", "DH", "DH", "DH", "H", "H", "H", "H", "H"],
   17 =>  ["DH", "DH", "DH", "DH", "DH", "H", "H", "H", "H", "H"],
-  18 =>  ["S", "DS", "DS", "DS", "DS", "S", "S", "H", "H", "S"],
-  19 =>  ["S", "S", "S", "S", "DS", "S", "S", "S", "S", "S"],
+  18 =>  ["S", "Ds", "Ds", "Ds", "Ds", "S", "S", "H", "H", "S"],
+  19 =>  ["S", "S", "S", "S", "Ds", "S", "S", "S", "S", "S"],
   20 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"],
   21 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"]
 }
@@ -42,10 +42,20 @@ player_pair = {
   10 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"],
   "A"  =>	 ["P", "P", "P", "P", "P", "P", "P", "P", "P", "P"]
 }
- #removed dealer card options - comments
 
-def check_if_valid(card)
-  until (2..10).include?(card.to_i) || ("J", "Q", "K", "A").include?(card.upcase)
+moves = {
+  "H" => "Hit",
+  "S" => "Stand",
+  "P" => "Split",
+  "Dh" => "Double if possible (otherwise hit)",
+  "Ds" => "Double if possible (otherwise stand)"
+}
+
+face_cards = ["J", "Q", "K", "A"]
+face_cards_no_ace = ["J", "Q", "K"]
+
+def check_if_valid(card, face_cards)
+  until (2..10).include?(card.to_i) || face_cards.include?(card.upcase)
     puts "Not valid entry, please try again"
     card = gets.chomp
   end
@@ -54,40 +64,61 @@ end
 def player_hand_type(card1, card2)
   if card1 == card2
     "pair"
-  elsif card1.upcase == "A" || card2.upcase == "A"
+  elsif card1 == 11 || card2 == 11
     "soft"
   else
     "hard"
+  end
 end
 
+def check_if_face(card, face_cards_no_ace) #include in comments
+  if face_cards_no_ace.include?(card.upcase)
+    card = 10
+  elsif card.upcase == "A"
+    card = 11
+  else
+    card.to_i
+  end
+end
 
-puts "Welcome to blackjack advisor! I'll tell you whether to hit, stand, split,
-      double if possible (otherwise hit), or double if possible (otherwise stand)"
+def move(hand_type, user_card1, user_card2, dealer_card1)
+  player_total = user_card1 + user_card2
+  hand_type[player_total][dealer_card1-2]
+end
+
+def new_game(move)
+  puts "Your optimal move is to #{move}"
+  puts "Would you like to play again? (y/n)"
+  gets.chomp
+end
+
+puts "Welcome to blackjack advisor! I'll tell you whether to hit, stand, split, double if possible (otherwise hit), or double if possible (otherwise stand)"
 
 loop do
   puts "Please enter your first card"
-  user_card1 = check_if_valid(gets.chomp)
+  user_card1 = check_if_valid(gets.chomp, face_cards)
   puts "Please enter your second card"
-  user_card2 = check_if_valid(gets.chomp)
+  user_card2 = check_if_valid(gets.chomp, face_cards)
   puts "Please enter the dealer's card"
-  dealer_card1 = check_if_valid(gets.chomp)
+  dealer_card1 = check_if_valid(gets.chomp, face_cards)
 
-hand_type = player_hand_type(user_card1, user_card2)
+  user_card1 = check_if_face(user_card1, face_cards_no_ace) #include in comments
+  user_card2 = check_if_face(user_card2, face_cards_no_ace)
+  dealer_card1 = check_if_face(dealer_card1, face_cards_no_ace)
 
-case hand_type
-  when "pair"
-    if card1.upcase == "A"
-      move = "P"
-    else
-      move = player_pair[card1.to_i][dealer_card1.to_i-2]
-    end
-  when "soft"
-  when "hard"
-end
+  player_hand_type = player_hand_type(user_card1, user_card2)
 
+  case player_hand_type
+    when "pair"
+      move = move(player_pair, user_card1, user_card2, dealer_card1)
+    when "soft"
+      move = move(player_soft, user_card1, user_card2, dealer_card1)
+    when "hard"
+      move = move(player_hard, user_card1, user_card2, dealer_card1)
+  end
 
-
-  puts "Your optimal move is to #{optimal_move}"
+  optimal_move = moves[move]
+  play_again = new_game(optimal_move)
 
   break unless play_again == "y"
 end
