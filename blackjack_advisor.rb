fbc4d9e6eec0c1c7d50fd88bcca634a46301a9a5
Benjamin Mangelsen <bmangelsen@lmao.local>
2016-10-13 14:01:38 -0400

    I added commas to pair hash, I added the card check_if_valid method, and I requested the cards from user

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -29,18 +29,45 @@ player_soft = {
   20 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"],
   21 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"]
 }
-player_pair = {
-  2  =>	 ["P", "P", "P", "P", "P", "P", "H", "H", "H", "H"]
-  3  =>	 ["P", "P", "P", "P", "P", "P", "P", "H", "H", "H"]
-  4  =>	 ["H", "H", "P", "P", "P", "H", "H", "H", "H", "H"]
-  5  =>	 ["Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "H", "H"]
-  6  =>	 ["P", "P", "P", "P", "P", "P", "H", "H", "H", "H"]
-  7  =>	 ["P", "P", "P", "P", "P", "P", "P", "H", "S", "H"]
-  8  =>	 ["P", "P", "P", "P", "P", "P", "P", "P", "P", "P"]
-  9  =>	 ["P", "P", "P", "P", "P", "S", "P", "P", "S", "S"]
-  10 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"]
-  "A"  =>	 ["P", "P", "P", "P", "P", "P", "P", "P", "P", "P"]
 
-}
+player_pair = {
+  2  =>	 ["P", "P", "P", "P", "P", "P", "H", "H", "H", "H"],
+  3  =>	 ["P", "P", "P", "P", "P", "P", "P", "H", "H", "H"],
+  4  =>	 ["H", "H", "P", "P", "P", "H", "H", "H", "H", "H"],
+  5  =>	 ["Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "Dh", "H", "H"],
+  6  =>	 ["P", "P", "P", "P", "P", "P", "H", "H", "H", "H"],
+  7  =>	 ["P", "P", "P", "P", "P", "P", "P", "H", "S", "H"],
+  8  =>	 ["P", "P", "P", "P", "P", "P", "P", "P", "P", "P"],
+  9  =>	 ["P", "P", "P", "P", "P", "S", "P", "P", "S", "S"],
+  10 =>  ["S", "S", "S", "S", "S", "S", "S", "S", "S", "S"],
+  "A"  =>	 ["P", "P", "P", "P", "P", "P", "P", "P", "P", "P"]
+} #include inserted commas in next commit
 
 dealer_card = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A"]
+
+
+def check_if_valid(card)
+  until (2..10).include?(card.to_i) || ("J", "Q", "K", "A").include?(card)
+    puts "Not valid entry, please try again"
+    card = gets.chomp
+  end
+end
+
+puts "Welcome to blackjack advisor! I'll tell you whether to hit, stand, split,
+      double if possible (otherwise hit), or double if possible (otherwise stand)"
+
+loop do
+  puts "Please enter your first card"
+  user_card1 = check_if_valid(gets.chomp)
+  puts "Please enter your second card"
+  user_card2 = check_if_valid(gets.chomp)
+  puts "Please enter the dealer's card"
+  dealer_card1 = check_if_valid(gets.chomp)
+
+  
+
+
+  puts "Your optimal move is to #{optimal_move}"
+
+  break unless play_again == "y"
+end
